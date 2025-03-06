2ef66518772dd2b41316a9c4d0fd03300ca0e525
dataengx <dataengx@mail.com>
2025-03-06 10:44:33 -0300

    add: dag example

---
+++ dags/simple_dag_with_dependencies.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Funções para as tarefas
+def task_1():
+    print("Tarefa 1")
+
+def task_2():
+    print("Tarefa 2")
+
+# Definindo o DAG
+dag = DAG(
+    'simple_dag_with_dependencies',
+    description='DAG com dependências entre tarefas',
+    schedule_interval=None,
+    start_date=datetime(2025, 3, 6),
+    catchup=False,
+)
+
+# Definindo as tarefas
+task_1_operator = PythonOperator(
+    task_id='task_1',
+    python_callable=task_1,
+    dag=dag,
+)
+
+task_2_operator = PythonOperator(
+    task_id='task_2',
+    python_callable=task_2,
+    dag=dag,
+)
+
+# Definindo a dependência entre as tarefas
+task_1_operator >> task_2_operator
